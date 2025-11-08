12495b28774dbd88c0f92e00b60f700185351473
sysmkr <to.sysmkr@gmail.com>
2025-11-08 22:31:31 +0100

    [09] ft_memcpy [T]

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vpolard <vpolard@student42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/08 20:44:27 by vpolard           #+#    #+#             */
-/*   Updated: 2025/11/08 22:19:50 by vpolard          ###   ########.fr       */
+/*   Created: 2025/11/08 22:20:00 by vpolard           #+#    #+#             */
+/*   Updated: 2025/11/08 22:30:56 by vpolard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*casted_dest;
+	const unsigned char	*casted_src;
+	size_t			index;
 
-# include <stdlib.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-size_t	ft_strlen(const char *s);
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-
-#endif
+	casted_dest = (unsigned char *)dest;
+	casted_src = (const unsigned char *)src;
+	index = 0;
+	while (index < n)
+	{
+		casted_dest[index] = casted_src[index];
+		index++;
+	}
+	return (dest);
+}
